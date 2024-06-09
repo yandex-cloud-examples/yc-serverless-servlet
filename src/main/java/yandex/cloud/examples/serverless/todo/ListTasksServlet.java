fbf34053a2a39eb90e29faa3b282723036817251
Dmitrii Karagodin <4319788@gmail.com>
2024-06-09 22:31:02 +0300

    Update for easy deploy to servlet container

--- src/main/java/yandex/cloud/examples/serverless/todo/ListTasksServlet.java
+++ src/main/java/yandex/cloud/examples/serverless/todo/ListTasksServlet.java
@@ -1,6 +1,6 @@
 package yandex.cloud.examples.serverless.todo;
 
-import com.jsoniter.output.JsonStream;
+import com.google.gson.Gson;
 import yandex.cloud.examples.serverless.todo.db.Dao;
 import yandex.cloud.examples.serverless.todo.db.TaskDao;
 import yandex.cloud.examples.serverless.todo.model.Task;
@@ -13,11 +13,12 @@ import java.io.IOException;
 public class ListTasksServlet extends HttpServlet {
 
     private final Dao<Task> taskDao = new TaskDao();
+    private final Gson gson = new Gson();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         var tasks = taskDao.findAll();
-        var tasksJsonString = JsonStream.serialize(tasks);
+        var tasksJsonString = gson.toJson(tasks);
 
         var out = resp.getWriter();
         resp.setContentType("application/json");
